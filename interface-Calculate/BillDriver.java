350fc4a3605c319efeb1ec1e5b116c435a5d68e9
Raashidvz <raashidvz313@gmail.com>
2026-02-20 21:51:55 +0530

    Update BillDriver.java

--- interface-Calculate/BillDriver.java
+++ interface-Calculate/BillDriver.java
@@ -1,5 +1,29 @@
 import java.util.Scanner;
 
 class BillDriver{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of products : ");
+        int n = sc.nextInt();
+        Product[] products = new Product[n];
 
+        for(int i=0;i<n;i++){
+            System.out.println("\nProduct "+(i+1));
+            System.out.print("ID : ");
+            int productID = sc.nextInt();
+            System.out.print("Name : ");
+            String name = sc.next();
+            System.out.print("Unit Price : ");
+            double unitPrice = sc.nextDouble();
+            System.out.print("Quantity : ");
+            int quantity = sc.nextInt();
+
+            products[i]=new Product(productID,quantity,name,unitPrice);
+        }
+
+        Bill bill = new Bill();
+        bill.calculate(products);
+        System.out.println("\n\nNet Amount : "+bill.netAmount);
+
+    }
 }
