bc1e65a6a3783002f4e8fb9a79c2cc2daaa62b25
Raashidvz <raashidvz313@gmail.com>
2026-03-18 12:23:26 +0530

    Update GStack.java

--- GenericClass/GStack.java
+++ GenericClass/GStack.java
@@ -1,16 +1,31 @@
 public class GStack<T> {
-	T arr[] = new T(100);
+	Object[] arr = new Object[100];
 	int top=-1;
 	
 	
-	boolean push(T e){
+	void push(T e){
 		arr[++top]=e;
 	}
 	
 	void pop(){
 		if(top==-1){
 			System.out.println("Stack Empty, nothing to delete");
-		}
+		}else{
+            System.out.println("Popped Element : "+arr[top]);
+        }
 		top--;
+        
 	}
+
+    void display(){
+        if(top==-1){
+            System.out.println("Stack is empty!");
+        }else{
+            System.out.println(" _______________");
+            for(int i=top;i>-1;i--){
+                System.out.println("|\t"+arr[i]+"\t|");
+            }
+            System.out.println(" ---------------");
+        }
+    }
 }
